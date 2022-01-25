ed322c538057d3e15cb30b3ded4755a707cf498f
Justine <98328323+JustineD22@users.noreply.github.com>
2022-01-25 15:47:01 +0100

    création de la boucle

---
+++ boucles.php
@@ -0,0 +1,13 @@
+<?php
+
+$resultat = "<ul>";
+
+for ($i = 0; $i < 100; $i++) {
+    $resultat .= "<li";
+    $resultat .= $i;
+    $resultat .= "</li>";
+}
+
+$resultat .= "</ul>";
+
+echo $resultat;
