7bbac7f0d8cbc4b37abeac8054abfb153b48a38e
Justine <98328323+JustineD22@users.noreply.github.com>
2022-01-27 09:35:09 +0100

    ajout fonction volume

--- fonctions.php
+++ fonctions.php
@@ -14,3 +14,8 @@ function surfaceRectangle (float $x, float $y): float
 {
     return abs($x) * abs($y);
 }
+
+function volume (float $x, float $y, float $z): float
+{
+    return abs($x) * abs($y) * abs($z);
+}
